dffe6e2fdf9c9fbfae7696f464d27a2244a582e2
Amy Cen <amy.cen@blackrock.com>
2019-06-03 14:39:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new(student[:name], student[:location])
+    end
   end
 
   def add_student_attributes(attributes_hash)
